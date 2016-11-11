c03884cc834728d00118a22865e5a945392bdd1a
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-11-11 15:56:44 +0200

    Cant understand how create init with model without designated initializers in AVExerciseTableViewCell

--- WorkoutTimer/AVExerciseDetailsTableViewCell.swift
+++ WorkoutTimer/AVExerciseDetailsTableViewCell.swift
@@ -12,6 +12,25 @@ class AVExerciseDetailsTableViewCell: UITableViewCell {
 
     @IBOutlet weak var exerciseNameLabel: UILabel!
     @IBOutlet weak var exerciseDurationLabel: UILabel!
+    var exerciseModel: AVExerciseModel
+
+    init(exerciseModel: AVExerciseModel) {
+        self.exerciseModel = exerciseModel
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.exerciseModel = AVExerciseModel(name: "none", duration: 0)
+        super.init(coder: aDecoder)
+    }
+    
+    func fillWithModel(model: AVExerciseModel) {
+        let seconds = model.exerciseDuration % 60
+//        var minutes = model.exerciseDuration > 60 ? model.exerciseDuration / 60 : 0
+        let minutes = model.exerciseDuration / 60
+        self.exerciseDurationLabel.text = "\(minutes) : \(seconds)"
+        self.exerciseNameLabel.text = model.exerciseName
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
